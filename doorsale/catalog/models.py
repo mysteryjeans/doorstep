ab69464b7a3e6a67651505d9d97ad2dadce83a13
Faraz Masood Khan <faraz@fanaticlab.com>
2014-04-08 02:31:32 +0500

    Catalog models

---
+++ doorsale/catalog/models.py
@@ -0,0 +1,125 @@
+from django.db import models
+
+
+class Manufacturer(models.Model):
+	"""
+	Represents a Manufacturer
+	"""
+	name = models.CharField(max_length=100, unique=True)
+	slug = models.SlugField(max_length=100, unique=True)
+	description = models.TextField(null=True, blank=True)
+	pic = models.ImageField(upload_to='images/catalog/manufacturers')
+	is_active = models.BooleanField(default=True)
+	updated_by = models.CharField(max_length=100)
+	updated_on = models.DateTimeField(auto_now=True)
+	created_on = models.DateTimeField(auto_now_add=True)
+	created_by = models.CharField(max_length=100)
+
+	def __unicode__(self):
+		return self.name
+
+
+class Category(models.Model):
+	"""
+	Represents a Category for Products
+	"""
+	name = models.CharField(max_length=100, unique=True)
+	slug = models.SlugField(max_length=100, unique=True)
+	description = models.TextField(null=True, blank=True)
+	pic = models.ImageField(upload_to='images/catalog/categories')
+	parent = models.ForeignKey('self', null=True, blank=True)
+	tags = models.CharField(max_length=100, null=True, blank=True, help_text='Comma-delimited set of SEO keywords for meta tag')
+	display_index = models.IntegerField(default=0)
+	is_active = models.BooleanField(default=True)
+	updated_by = models.CharField(max_length=100)
+	updated_on = models.DateTimeField(auto_now=True)
+	created_on = models.DateTimeField(auto_now_add=True)
+	created_by = models.CharField(max_length=100)
+
+	class Meta:
+		db_table = 'catalog_categories'
+        ordering = ('display_index', 'id',)
+        verbose_name_plural = 'Categories'
+
+	def __unicode__(self):
+		if self.parent:
+			return u"%s > %s" % (self.parent, self.name)
+
+		return self.name
+
+
+class Product(models.Model):
+	"""
+	Represents a Product
+	"""
+	name = models.CharField(max_length=100, unique=True)
+	slug = models.SlugField(max_length=100, unique=True)
+	brand = models.ForeignKey(Manufacturer)
+	sku = models.CharField(max_length=50, null=True, blank=True)
+	gist = models.CharField(max_length=500, null=True, blank=True, help_text='Short description of the Product')
+	description = models.TextField(null=True, blank=True)
+	price = models.DecimalField(max_digits=9,decimal_places=2)
+    old_price = models.DecimalField(max_digits=9,decimal_places=2, blank=True,default=0.00)
+    is_active = models.BooleanField(default=True)
+    is_bestseller = models.BooleanField(default=False)
+    is_featured = models.BooleanField(default=False)
+    quantity = models.IntegerField()
+    tax_rate = models.FloatField(default=0.0, help_text='Tax rate in percentage %: 0.02')
+    rating  = models.FloatField(default=0.0)
+    up_votes = models.IntegerField(default=0)
+    down_votes = models.IntegerField(default=0)
+	categories = models.ManyToManyField(Category)
+	tags = models.CharField(max_length=250, null=True, blank=True, help_text='Comma-delimited set of SEO keywords for meta tag')
+	updated_by = models.CharField(max_length=100)
+	updated_on = models.DateTimeField(auto_now=True)
+	created_on = models.DateTimeField(auto_now_add=True)
+	created_by = models.CharField(max_length=100)
+
+	class Meta:
+		ordering = ('id',)
+
+	def __unicode__(self):
+		return self.name
+
+
+class ProductSpec(models.Model):
+	"""
+	Represents product specification attribute
+	"""
+	name = models.CharField(max_length=100)
+	product = models.ForeignKey(Product, related_name='specs')
+	value = models.CharField(max_length=100)
+	display_index = models.IntegerField(default=0)
+	created_on = models.DateTimeField(auto_now_add=True)
+	created_by = models.CharField(max_length=100)
+
+	class Meta:
+		db_table = 'catalog_product_specs'
+		ordering = ('display_index', 'name',)
+		unique_together = ('name', 'product',)
+		verbose_name_plural = 'Product Specs'
+
+
+class ProductPic(models.Model):
+	"""
+	Represents product picture
+	"""
+	name = models.CharField(max_length=100)
+	product = models.ForeignKey(Product, related_name='pics')
+	url = models.ImageField(upload_to="images/catalog/products")
+	created_on = models.DateTimeField(auto_now_add=True)
+	created_by = models.CharField(max_length=100)
+
+	class Meta:
+		db_table = "catalog_product_pics"
+		unique_together = ('name', 'product',)
+		verbose_name_plural = "Product Pics"
+
+	def __unicode__(self):
+		return self.name
+
+
+
+		
+
+
