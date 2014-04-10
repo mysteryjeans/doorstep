f55c4cb55b1bcbe95a80ae89cd9ebcbbab4e002f
Faraz Masood Khan <faraz@fanaticlab.com>
2014-04-11 02:19:08 +0500

    Country model added

--- doorsale/common/models.py
+++ doorsale/common/models.py
@@ -1,6 +1,25 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+
+class Country(models.Model):
+    """
+    Represents a country 
+    """
+    name = models.CharField(max_length=100, unique=True)
+    allow_billing = models.BooleanField(default=True, help_text='Allow billing from this country')
+    allow_shipping = models.BooleanField(default=True, help_text='Allow shipping to this country')
+    iso_code2 = models.CharField(max_length=2, unique=True, help_text='Two letter ISO code')
+    iso_code3 = models.CharField(max_length=3, unique=True, help_text='Three letter ISO code')
+    iso_numerice = models.IntegerField(help_text='Numeric ISO code')
+    subject_to_vat = models.BooleanField(default=False, help_text='Is VAT applicable')
+    display_order = models.IntegerField(default=0)
+    is_active = models.BooleanField(default=True)
+    
+    def __unicode__(self):
+        return self.name 
+    
+
 class BaseUser(AbstractUser):
     """
     An abstract class extending Django auth user model for doorsale.
