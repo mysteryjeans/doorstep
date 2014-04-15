0e7184e77ab7157861114337af9e56e5bd1473a3
Faraz Masood Khan <faraz@fanaticlab.com>
2014-04-15 13:05:04 +0500

    Fixed auto fill created_by and updated_by fields in django admin

--- doorsale/admin.py
+++ doorsale/admin.py
@@ -8,10 +8,11 @@ class ModelAdmin(admin.ModelAdmin):
     exclude = ('updated_by', 'created_by',)
     def save_form(self, request, form, change):
         obj = super(ModelAdmin, self).save_form(request, form, change)
-        if not change:
-            if hasattr(obj, 'updated_by'):
-                obj.updated_by = unicode(request.user)
+    
+        if hasattr(obj, 'updated_by'):
+            obj.updated_by = unicode(request.user)
+       
+        if hasattr(obj, 'created_by') and not obj.created_by:
+            obj.created_by = unicode(request.user)
             
-            if hasattr(obj, 'created_by'):
-                obj.created_by = unicode(request.user)
-        return obj    
+        return obj     
