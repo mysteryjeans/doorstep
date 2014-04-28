c649898bda89174c70a191e3f65e46169fbf3669
Faraz Masood Khan <faraz@fanaticlab.com>
2014-04-29 03:00:30 +0500

    Product details overview and specifications

--- doorsale/common/settings.py
+++ doorsale/common/settings.py
@@ -13,6 +13,7 @@ PIPLELINE_JS = {
     'base': {
         'source_filenames': (
           'common/scripts/jquery-1.11.0.js',
+          'common/scripts/jquery-ui-1.10.4.custom.js',
         ),
         'output_filename': 'common/scripts/base.js',
     },
