32209509b178c2f90a93dc5a9af5caf1b6e2bb66
Faraz Masood Khan <faraz@fanaticlab.com>
2014-05-01 21:40:48 +0500

    Catalog filters cleanup

--- doorsale/catalog/templatetags/catalog_filters.py
+++ doorsale/catalog/templatetags/catalog_filters.py
@@ -1,20 +1,17 @@
-import locale 
-
 from django import template 
 from django.core.exceptions import ImproperlyConfigured
 
+
 register = template.Library()
-locale.setlocale(locale.LC_ALL, 'en_US')
+
 
 @register.filter(name='currency')
 def currency(price, currency):
     """
     Returns price in currency format
     """
-    price = float(price) * currency.exchange_rate
+    price = float(price) * float(currency.exchange_rate)
     try:
-        print currency.display_format
         return currency.display_format.format(price)
     except Exception as e:
-        raise ImproperlyConfigured('Invalid currency format string: "%s" for currency "%s". ' % (currency.display_format, currency.name)
-                                   + e.message )
+        raise ImproperlyConfigured('Invalid currency format string: "%s" for currency "%s". %s' % (currency.display_format, currency.name, e.message))
