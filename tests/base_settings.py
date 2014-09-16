13e506fc771c11f790404836bc51cc059ab1e9de
Faraz Masood Khan <faraz@fanaticlab.com>
2014-09-16 05:57:37 +0500

    Fixing lazy translation call for test

--- tests/base_settings.py
+++ tests/base_settings.py
@@ -46,7 +46,7 @@ ROOT_URLCONF = 'doorsale.urls'
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+#LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
 
