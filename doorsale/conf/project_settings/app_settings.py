66411198dca250eb13ab7dd7d1facaceadd6c700
Faraz Masood Khan <faraz@fanaticlab.com>
2014-09-20 19:07:42 +0500

    Static files and media settings included in site template

--- doorsale/conf/project_settings/app_settings.py
+++ doorsale/conf/project_settings/app_settings.py
@@ -1,4 +1,10 @@
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static', 'media')
+
 
 ################## Doorsale #########################
 #####################################################
