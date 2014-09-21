a468c77f2e230476021274af55232fa9fb488b85
Faraz Masood Khan <faraz@fanaticlab.com>
2014-09-21 18:51:06 +0500

    Django base version schema

--- doorsale/__init__.py
+++ doorsale/__init__.py
@@ -1,6 +1,6 @@
 VERSION = (0, 1, 0, 'alpha', 0)
 
 
-def get_version(*args, **kwargs):
+def get_version():
     from doorsale.utils.version import get_version
-    return get_version(*args, **kwargs)
+    return get_version(VERSION)
--- doorsale/utils/version.py
+++ doorsale/utils/version.py
@@ -1,5 +1,13 @@
+from __future__ import unicode_literals
 
-# Copied from Django
+import datetime
+import os
+import subprocess
+
+
+# Returns PEP 386-compliant version for doorsale
+# This code is copied from django.utils.version branch stable/1.6.x
+
 def get_version(version=None):
     "Returns a PEP 386-compliant version number from VERSION."
     if version is None:
@@ -17,9 +25,32 @@ def get_version(version=None):
     main = '.'.join(str(x) for x in version[:parts])
 
     sub = ''
+    if version[3] == 'alpha' and version[4] == 0:
+        git_changeset = get_git_changeset()
+        if git_changeset:
+            sub = '.dev%s' % git_changeset
 
-    if version[3] != 'final':
+    elif version[3] != 'final':
         mapping = {'alpha': 'a', 'beta': 'b', 'rc': 'c'}
         sub = mapping[version[3]] + str(version[4])
 
     return str(main + sub)
+
+
+def get_git_changeset():
+    """Returns a numeric identifier of the latest git changeset.
+
+    The result is the UTC timestamp of the changeset in YYYYMMDDHHMMSS format.
+    This value isn't guaranteed to be unique, but collisions are very unlikely,
+    so it's sufficient for generating the development version numbers.
+    """
+    repo_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    git_log = subprocess.Popen('git log --pretty=format:%ct --quiet -1 HEAD',
+                               stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                               shell=True, cwd=repo_dir, universal_newlines=True)
+    timestamp = git_log.communicate()[0]
+    try:
+        timestamp = datetime.datetime.utcfromtimestamp(int(timestamp))
+    except ValueError:
+        return None
+    return timestamp.strftime('%Y%m%d%H%M%S')
