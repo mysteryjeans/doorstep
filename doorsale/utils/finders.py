a368d12bc2eab630dfad373f6a4d9e973813f37f
Faraz Masood Khan <faraz@fanaticlab.com>
2014-09-24 12:58:52 +0500

    LESS import converted to relative paths

---
+++ doorsale/utils/finders.py
@@ -0,0 +1,32 @@
+import os
+
+
+def get_app_paths(app_names):
+    """
+    Returns list of app directory paths
+    """
+    from django.utils.importlib import import_module
+
+    app_paths = []
+
+    for app_name in app_names:
+        mod = import_module(app_name)
+        app_path = os.path.dirname(mod.__file__)
+        app_paths.append(app_path)
+
+    return app_paths
+
+
+def get_static_paths(app_names):
+    """
+    Returns list of static folders path of specified apps
+    """
+    static_paths = []
+
+    for app_dir in get_app_paths(app_names):
+        app_static_dir = os.path.join(app_dir, 'static')
+        
+        if os.path.exists(app_static_dir):
+            static_paths.append(app_static_dir)
+
+    return static_paths
