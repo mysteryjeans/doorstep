600e06d29f06915dd4cb0aa38f29f6098c88c4a4
Faraz M. Khan <mk.faraz@gmail.com>
2018-03-04 22:14:13 +0500

    Cart basket materialized

--- doorstep/catalog/static/catalog/scripts/jquery.catalog_base.js
+++ doorstep/catalog/static/catalog/scripts/jquery.catalog_base.js
@@ -1,23 +1,10 @@
-$('[parent-category]').each(function() {
-	var $sub_categories = $(this);
-	$($sub_categories.attr('parent-category')).click(function() {
-		if ($sub_categories.is(':visible')) {
-			$sub_categories.hide(100);
-			$(this).removeClass('arrow-down');
-		} else {
-			$sub_categories.show(100);
-			$(this).addClass('arrow-down');
-		}
-	});
-});
-
 
 function basketChanged() {
-	$('#basket-container .price').hide();
-	$('#basket-container .price').fadeIn('fast');
-	
+	$('#basket-dropdown-button').dropdown();
+	$('#basket-dropdown').appendTo($('main#content'));
 	// Initializing new forms in basket-container
-	bootstrapAjax($, '#basket-container');
+	bootstrapAjax($, '#basket-dropdown');
 }
-
-$('select:not([multiple])').material_select();
+$(document).ready(function() {
+	$('#basket-dropdown').appendTo($('main#content'));
+});
