5ccd95fda745459c095f063598237d52d8db73eb
Faraz M. Khan <mk.faraz@gmail.com>
2018-03-18 08:02:10 +0500

    Redesign product detail page with bootstrap

--- doorstep/static/doorstep/scripts/base.js
+++ doorstep/static/doorstep/scripts/base.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+  $('[data-toggle="tooltip"]').tooltip();
   $(document).on('click', '[data-href]:not(a)', function() {
     window.location = $(this).attr('data-href');
   });
