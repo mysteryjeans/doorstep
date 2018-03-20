b2061b5e3c157231b8dfba3fbb2c4864cb8a3891
Faraz M. Khan <mk.faraz@gmail.com>
2018-03-20 10:10:56 +0500

    Checkout pages for sales app

--- doorstep/sales/views.py
+++ doorstep/sales/views.py
@@ -366,11 +366,11 @@ class CheckoutOrderView(CheckoutBaseView):
             order = Order.objects.place(cart_id, billing_address_id, shipping_address_id,
                                         payment_method, po_number, currency_code, user, username)
 
-            # del request.session['cart_id']
-            # del request.session['payment_method']
-            # del request.session['billing_address']
-            # del request.session['shipping_address']
-            # request.session['order_confirmed'] = True
+            del request.session['cart_id']
+            del request.session['payment_method']
+            del request.session['billing_address']
+            del request.session['shipping_address']
+            request.session['order_confirmed'] = True
 
             if payment_method == PaymentMethod.CREDIT_CARD:
                 return HttpResponseRedirect(reverse('payments_process_online', args=[order.id, order.receipt_code]))
